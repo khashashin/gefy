5bb0c24e91af2047d57014a9faeb63caa957a4e1
khashashin <khashashin@gmail.com>
2022-02-15 22:55:02 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# gefy
+# How to upload Photos
+
+1. Copy the photos (*.jpg) inside your Folder
+   1. Franz: `assets/img/f`
+   2. Eleasar: `assets/img/e`
+2. Update the `data.json` file:
+
+Before:
+```
+{
+    "e": [],
+    "f": [],
+    "y": [{
+        "data": "Name: Khasbulatov\n",
+        "path": "assets/img/y/y1.jpg"
+    }, {
+        "data": "Name: Khasbulatov\n",
+        "path": "assets/img/y/y2.jpg"
+    }, {
+        "data": "Name: Khasbulatov\n",
+        "path": "assets/img/y/y3.jpg"
+    }]
+}
+```
+
+After
+```
+{
+    "e": [{
+        "data": "Name: Blum\n",
+        "path": "assets/img/e/e1.jpg"
+    }, {
+        "data": "Name: Blum\n",
+        "path": "assets/img/e/e2.jpg"
+    }, {
+        "data": "Name: Blum\n",
+        "path": "assets/img/e/e3.jpg"
+    }],
+    "f": [{
+        "data": "Name: Kilchenmann\n",
+        "path": "assets/img/f/f1.jpg"
+    }, {
+        "data": "Name: Kilchenmann\n",
+        "path": "assets/img/f/f2.jpg"
+    }, {
+        "data": "Name: Kilchenmann\n",
+        "path": "assets/img/f/f3.jpg"
+    }],
+    "y": [{
+        "data": "Name: Khasbulatov\n",
+        "path": "assets/img/y/y1.jpg"
+    }, {
+        "data": "Name: Khasbulatov\n",
+        "path": "assets/img/y/y2.jpg"
+    }, {
+        "data": "Name: Khasbulatov\n",
+        "path": "assets/img/y/y3.jpg"
+    }]
+}
+```
--- js/main.js
+++ js/main.js
@@ -11,7 +11,10 @@ function main() {
     }).then(data => {
         console.log(data);
         const dataY = data.y;
+        const dataF = data.f;
+        const dataE = data.e;
 
+        // Yusuf
         for (let i = 0; i < dataY.length; i++) {
             let tr = document.createElement('tr');
             let tdData = document.createElement('td');
@@ -22,6 +25,40 @@ function main() {
 
             let img = document.createElement('img');
             img.src = dataY[i].path;
+            tdImage.appendChild(img);
+            tr.appendChild(tdData);
+            tr.appendChild(tdImage);
+            tbody.appendChild(tr);
+        }
+
+        // Franz
+        for (let i = 0; i < dataF.length; i++) {
+            let tr = document.createElement('tr');
+            let tdData = document.createElement('td');
+            let tdImage = document.createElement('td');
+            tdImage.classList.add('image');
+
+            tdData.innerHTML = dataF[i].data;
+
+            let img = document.createElement('img');
+            img.src = dataF[i].path;
+            tdImage.appendChild(img);
+            tr.appendChild(tdData);
+            tr.appendChild(tdImage);
+            tbody.appendChild(tr);
+        }
+
+        // Eleasar
+        for (let i = 0; i < dataE.length; i++) {
+            let tr = document.createElement('tr');
+            let tdData = document.createElement('td');
+            let tdImage = document.createElement('td');
+            tdImage.classList.add('image');
+
+            tdData.innerHTML = dataE[i].data;
+
+            let img = document.createElement('img');
+            img.src = dataE[i].path;
             tdImage.appendChild(img);
             tr.appendChild(tdData);
             tr.appendChild(tdImage);
