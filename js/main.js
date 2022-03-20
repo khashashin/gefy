9e9494705408205848c35ec0b2d2d97d3c131120
Yusuf Khasbulatov <khashashin@gmail.com>
2022-03-20 23:16:53 +0100

    changes the layout as it was discussed in chat. This commit ships not all functionality and only covers Mobile design.

--- js/main.js
+++ js/main.js
@@ -1,70 +1,106 @@
 document.addEventListener('DOMContentLoaded', main, false);
 
+const body = document.querySelector('body');
+const html = document.querySelector('html');
+
 function main() {
-    let tbody = document.querySelector('tbody');
+    let container = document.querySelector('.container');
 
-
-    fetch('assets/data.json', {
+    fetch('assets/data-v2.json', {
         method: 'GET',
     }).then(response => {
         return response.json();
     }).then(data => {
-        console.log(data);
-        const dataY = data.y;
-        const dataF = data.f;
-        const dataE = data.e;
+        for (let i = 0; i < data.length; i++) {
 
-        // Yusuf
-        for (let i = 0; i < dataY.length; i++) {
-            let tr = document.createElement('tr');
-            let tdData = document.createElement('td');
-            let tdImage = document.createElement('td');
-            tdImage.classList.add('image');
+            let card = `
+                <div class="card" data-path="${data[i].path}" data-path-edited="${data[i].pathEdited}">
+                    <div class="card-body">
+                        <img src="${data[i].path}" alt="${data[i].data}">
+                    </div>
+                    <div class="card-footer">
+                        <p>${data[i].data}</p>
+                        <img class="icon" src="../assets/icons/info.svg">
+                    </div>
+                </div>
+            `;
 
-            tdData.innerHTML = dataY[i].data;
-
-            let img = document.createElement('img');
-            img.src = dataY[i].path;
-            tdImage.appendChild(img);
-            tr.appendChild(tdData);
-            tr.appendChild(tdImage);
-            tbody.appendChild(tr);
-        }
-
-        // Franz
-        for (let i = 0; i < dataF.length; i++) {
-            let tr = document.createElement('tr');
-            let tdData = document.createElement('td');
-            let tdImage = document.createElement('td');
-            tdImage.classList.add('image');
-
-            tdData.innerHTML = dataF[i].data;
-
-            let img = document.createElement('img');
-            img.src = dataF[i].path;
-            tdImage.appendChild(img);
-            tr.appendChild(tdData);
-            tr.appendChild(tdImage);
-            tbody.appendChild(tr);
-        }
-
-        // Eleasar
-        for (let i = 0; i < dataE.length; i++) {
-            let tr = document.createElement('tr');
-            let tdData = document.createElement('td');
-            let tdImage = document.createElement('td');
-            tdImage.classList.add('image');
-
-            tdData.innerHTML = dataE[i].data;
-
-            let img = document.createElement('img');
-            img.src = dataE[i].path;
-            tdImage.appendChild(img);
-            tr.appendChild(tdData);
-            tr.appendChild(tdImage);
-            tbody.appendChild(tr);
+            container.innerHTML += card;
         }
     }).catch(error => {
         console.log(error);
-    });
+    }).finally(() => {
+        createEventListeners();
+    })
+}
+
+function createEventListeners() {
+    const cards = document.querySelectorAll('.card');
+    
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', function() {
+            const data = {
+                path: this.getAttribute('data-path'),
+                pathEdited: this.getAttribute('data-path-edited'),
+            }
+
+            openModal(data);
+
+            const modal = document.querySelector('.modal');
+            const closeIcon = modal.querySelector('.icon.close');
+            const compareIcon = modal.querySelector('.icon.compare');
+            const slideIcon = modal.querySelector('.icon.slide');
+            closeIcon.addEventListener('click', closeModal);
+            compareIcon.addEventListener('click', comparePhoto(data));
+            slideIcon.addEventListener('click', slidePhoto(data));
+        });
+    }
+}
+
+function openModal(data) {
+
+    body.style.overflowX = 'hidden';
+    body.style.overflowY = 'auto';
+    html.style.overflowX = 'hidden';
+    html.style.overflowY = 'auto';
+
+    const modal = document.querySelector('.modal');
+    const img = document.createElement('img');
+    const imgEdited = document.createElement('img');
+    img.classList.add('photo');
+    imgEdited.classList.add('photo');
+
+    if (modal) {
+        img.src = data.path;
+        imgEdited.src = data.pathEdited;
+        modal.querySelector('.modal-body').appendChild(img);
+        modal.querySelector('.modal-body').appendChild(imgEdited);
+        modal.style.display = 'block';
+    }
+}
+
+function closeModal() {
+
+    body.style.overflowX = 'auto';
+    body.style.overflowY = 'auto';
+    html.style.overflowX = 'auto';
+    html.style.overflowY = 'auto';
+
+    const modal = document.querySelector('.modal');
+    if (modal) {
+        modal.style.display = 'none';
+        modal.querySelectorAll('.photo')
+            .forEach(photo => {
+                photo.remove();
+            }
+        );
+    }
+}
+
+function comparePhoto(data) {
+    
+}
+
+function slidePhoto(data) {
+    
 }
